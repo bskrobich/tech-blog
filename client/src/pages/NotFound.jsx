30be6b1a969d2f36d7a9b89a8389bded65951275
Bartosz Skrobich <55624515+bskrobich@users.noreply.github.com>
2025-01-05 17:02:17 +0100

    Add 404 Not Found page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function NotFound() {
     return (
-        <div>Page Not Found</div>
+        <div className="not-found">
+            Oops... You are in the wrong place. Page Not Found.
+        </div>
     );
 }
 
