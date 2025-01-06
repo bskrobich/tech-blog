089ffdb9a5cd82fd9e283cd1540cdd899c07237a
Bartosz Skrobich <55624515+bskrobich@users.noreply.github.com>
2025-01-06 21:22:42 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Layout />} >
-                            <Route path="/" element={<Home />} />
+                            <Route index element={<Home />} />
                             <Route path="/write" element={<Write />} />
                             <Route path="/post/:id" element={<Single />} />
                         </Route>
