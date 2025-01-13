4140dc3e41257efb6489fbe6beeb151471fc879e
Bartosz Skrobich <55624515+bskrobich@users.noreply.github.com>
2025-01-13 18:11:11 +0100

    Implement single post view by id from db

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -10,20 +10,37 @@ export const getPosts = async (req, res) => {
 
     db.query(getPostsQuery, queryParams, (err, result) => {
         if (err) {
-            return res.send(err);
+            return res.status(500).send(err);
         }
         return res.status(200).json(result.rows);
     });
 }
-export const getPost = async (req, res) => {
 
+export const getPost = async (req, res) => {
+    const postId = req.params.id;
+    const getPostQuery =
+        `SELECT p.title, p.content, p.image_url, p.updated_at, p.user_id, u.username AS author, c.name AS category
+         FROM "user" u 
+         JOIN post p ON p.user_id = u.id 
+         JOIN category c ON p.category_id = c.id
+         WHERE p.id = $1`;
+
+    db.query(getPostQuery, [postId], (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        return res.status(200).json(result.rows[0]);
+    });
 }
+
 export const addPost = async (req, res) => {
 
 }
+
 export const updatePost = async (req, res) => {
 
 }
+
 export const deletePost = async (req, res) => {
 
 }
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,44 +1,63 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Edit from '../images/edit.png'
 import Delete from '../images/delete.png'
-import {Link} from "react-router";
+import {Link, useLocation} from "react-router";
 import Menu from "../components/Menu.jsx";
+import axios from "axios";
+import moment from "moment";
+import {AuthContext} from "../context/authContext.jsx";
 
 function Single() {
+
+    const [post, setPost] = useState({});
+
+    const postId = useLocation().pathname.split("/")[2];
+
+    const { user } = useContext(AuthContext);
+
     const comments =
         [
-        "Great article!",
-        "Very informative.",
-        "I totally agree with this point of view!"
-    ];
+            "Great article!",
+            "Very informative.",
+            "I totally agree with this point of view!"
+        ];
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/api/posts/${postId}`);
+                setPost(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [postId]);
+
     return (
         <div className="single">
             <div className="container">
                 <div className="content">
                     <img
-                        src="https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1334/750x500/filters:focal(1020x667:1021x668):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/25334825/STK466_ELECTION_2024_CVirginia_E.jpg"
+                        src={post?.image_url}
                         alt=""/>
                     <div className="group-container">
                         <div className="user">
-                            <span><b>Bartosz S.</b></span>
-                            <p>Posted 3 days ago</p>
+                            <span><b>{post?.author}</b></span>
+                            <p>Posted {moment(post?.updated_at).fromNow()}</p>
                         </div>
-                        <div className="edit">
+                        { parseInt(user?.id) === post?.user_id && <div className="edit">
                             <Link to="/write?edit=1">
                                 <img src={Edit} alt="Edit"/>
                             </Link>
                             <img src={Delete} alt="Delete"/>
-                        </div>
+                        </div> }
                     </div>
                     <h1>
-                        Trump asks the Supreme Court to let him rescue TikTok
+                        {post?.title}
                     </h1>
                     <p>
-                        President-elect Donald Trump is asking the Supreme Court to let him negotiate a deal to save TikTok from an imminent US ban.
-                        <br/><br/>
-                        In an amicus brief filed to the court, Trump says he “seeks the ability to resolve the issues at hand through political means once he takes office,” and that he “alone possesses the consummate dealmaking expertise, the electoral mandate, and the political will to negotiate a resolution to save the platform.”
-                        <br/><br/>
-                        Last week, the Supreme Court agreed to hear arguments that a bill passed by Congress banning TikTok on national security grounds violates the First Amendment. The bill gives wide latitude to the president to delay its enforcement if there’s progress being made towards a deal that ensures TikTok isn’t fully controlled by its Chinese parent company, ByteDance.
+                        {post?.content}
                     </p>
                 </div>
                 <div className="menu">
