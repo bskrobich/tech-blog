36c08cd6789b05ca0d9443bac5ef65dc9b279cf0
Bartosz Skrobich <55624515+bskrobich@users.noreply.github.com>
2025-01-15 18:27:21 +0100

    Implement uploading image file to a server

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.js';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
+import multer from 'multer';
 
 const app = express();
 
@@ -14,6 +15,21 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../client/public/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+app.post('/api/upload', upload.single('image'), function (req, res) {
+    const image = req.file;
+    res.status(200).json(image.filename);
+})
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import axios from "axios";
 
 function Write() {
-    const [value, setValue] = useState('');
+    const [content, setContent] = useState('');
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState(null);
+    const [category, setCategory] = useState('');
+
+    const upload = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('image', image);
+            const res = await axios.post('http://localhost:3000/api/upload', formData);
+            return res.data;
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        upload();
+    }
+
     return (
         <div className="write">
             <div className="content">
-                <input placeholder="Title" />
+                <input type="text" placeholder="Title" onChange={e => setTitle(e.target.value)} />
                 <div className="text-container">
-                    <ReactQuill className="text" theme="snow" value={value} onChange={setValue} />
+                    <ReactQuill className="text" value={content} onChange={setContent} />
                 </div>
             </div>
             <div className="menu">
@@ -18,33 +39,33 @@ function Write() {
                     <span>
                         <b>Status: </b>Draft
                     </span>
-                    <input style={{display:"none"}} type="file" name="" id="file"/>
+                    <input style={{display:"none"}} type="file" name="" id="file" onChange={e => setImage(e.target.files[0])} />
                     <label className="upload" htmlFor="file">Upload Image</label>
                     <div className="buttons">
                         <button>Save draft</button>
-                        <button>Publish</button>
+                        <button onClick={handleClick}>Publish</button>
                     </div>
                 </div>
                 <div className="item">
                     <h1>Category</h1>
                     <div className="category">
-                        <input type="radio" name="category" value="tech" id="tech"/>
+                        <input type="radio" name="category" value="tech" id="tech" onChange={e => setCategory(e.target.value)}/>
                         <label htmlFor="tech">Tech</label>
                     </div>
                     <div className="category">
-                        <input type="radio" name="category" value="reviews" id="reviews"/>
+                        <input type="radio" name="category" value="reviews" id="reviews" onChange={e => setCategory(e.target.value)}/>
                         <label htmlFor="reviews">Reviews</label>
                     </div>
                     <div className="category">
-                        <input type="radio" name="category" value="science" id="science"/>
+                        <input type="radio" name="category" value="science" id="science" onChange={e => setCategory(e.target.value)}/>
                         <label htmlFor="science">Science</label>
                     </div>
                     <div className="category">
-                        <input type="radio" name="category" value="entertainment" id="entertainment"/>
+                        <input type="radio" name="category" value="entertainment" id="entertainment" onChange={e => setCategory(e.target.value)}/>
                         <label htmlFor="entertainment">Entertainment</label>
                     </div>
                     <div className="category">
-                        <input type="radio" name="category" value="ai" id="ai"/>
+                        <input type="radio" name="category" value="ai" id="ai" onChange={e => setCategory(e.target.value)}/>
                         <label htmlFor="ai">AI</label>
                     </div>
                 </div>
